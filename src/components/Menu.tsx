4338fe11c91724c84729412eeaf654b1690aef0f
ntimaks <ntimaks27@gmail.com>
2024-11-27 15:25:27 +0100

    fixed x

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -2,14 +2,13 @@ import Hamburger from "./hamburger";
 import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from "./ui/sheet";
 
 
+
 export default function Menu() {
     return (
         <Sheet>
             <SheetTrigger><Hamburger size={50} /></SheetTrigger>
             <SheetContent side="left" >
-                <SheetHeader>
-                    <SheetTitle>MENU</SheetTitle>
-                </SheetHeader>
+                <h1>MENU</h1>
             </SheetContent>
         </Sheet>
     );
--- src/components/hamburger.tsx
+++ src/components/hamburger.tsx
@@ -1,15 +1,16 @@
 import { motion } from "framer-motion";
 
+
 export default function Hamburger({ size }: { size?: number }) {
   return (
-    <svg 
-      xmlns="http://www.w3.org/2000/svg" 
-      height={size ?? '48'} 
-      viewBox="0 -960 960 960" 
-      width={size ?? '48'} 
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      height={size ?? '48'}
+      viewBox="0 -960 960 960"
+      width={size ?? '48'}
       fill="white"
     >
-      <path d="M99-205v-92h763v92H99Zm0-229v-91h763v91H99Zm0-229v-92h763v92H99Z"/>
+      <path d="M99-205v-92h763v92H99Zm0-229v-91h763v91H99Zm0-229v-92h763v92H99Z" />
     </svg>
   );
 }
